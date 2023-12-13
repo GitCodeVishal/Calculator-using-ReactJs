79becd2a19d460ee4ed7675087ced4799f93cb6a
GitCodeVishal <vkashyap1998@gmail.com>
2023-12-13 19:05:17 +0530

    Created react app and components

---
+++ calculator/src/App.js
@@ -0,0 +1,16 @@
+import Display from "./Display";
+import Buttons from "./Buttons";
+import Signs from "./Signs";
+
+
+function App() {
+  return (
+    <div className="App">
+     <Display/>
+     <Signs/>
+     <Buttons/>
+    </div>
+  );
+}
+
+export default App;
